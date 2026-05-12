cd4c810f30ff11b4b1572b6ded42acc2484253c3
Bronya-Zaychik0818 <bronya@example.com>
2026-05-12 20:31:18 +0800

    最终版本：梅比乌斯与希希芙

--- Mobius.h
+++ Mobius.h
@@ -34,21 +34,32 @@ protected:
     void keyPressEvent(QKeyEvent* event) override;
 
 private:
-    enum class GameState { WaitingToStart, Playing, GameOver };
+    QPixmap stickerGreenWin;
+    QPixmap stickerYellowWin;
+    QList<QPoint> snake2;
     enum Direction { Up, Down, Left, Right };
+    Direction dir2;
+    int score2;
+    enum class GameState { WaitingToStart, Playing, GameOver };
     QList<QPoint> obstacles;
+    QList<QPoint> foods;
+    int foodCount = 5;
     int obstacleCount = 3;
     void spawnObstacles();
     void initGame();
     void updateGame();
-    void spawnFood();
     void startBackgroundMusic();
     void stopBackgroundMusic();
+    QTimer* countdownTimer;
+    int timeLeft;
     QSoundEffect* m_backgroundMusic;
     QTimer* timer;
     QList<QPoint> snake;
     Direction dir;
-    QPoint food;
+    void respawnSnake1();
+    void respawnSnake2();
+    void spawnOneFood(); 
+    void updateCountdown();
     bool isGameOver;
     GameState gameState;
     int score;
